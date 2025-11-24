4d79e89a404596216b4a7280f9aefcfd832bd093
Masab1995 <73560556+Masab1995@users.noreply.github.com>
2025-11-24 17:41:41 +0000

    Refactor customer_sales.sql to use CTEs for limits

--- models/customer_sales.sql
+++ models/customer_sales.sql
@@ -1,10 +1,21 @@
 {{ config(materialized="table") }}
 
+with limited_data as (
+    select *
+    from {{ source('tpcds', 'store_sales') }}
+    limit 10
+),
+customer_subset as (
+    select *
+    from {{ source('tpcds', 'customer') }}
+    limit 10
+)
+
 select
     c.C_CUSTOMER_SK as customer_id,
     sum(s.SS_SALES_PRICE) as total_sales,
     count(*) as order_count
-from {{ source('tpcds', 'store_sales') }} s
-join {{ source('tpcds', 'customer') }} c
+from limited_data s
+join customer_subset c
     on s.SS_CUSTOMER_SK = c.C_CUSTOMER_SK
 group by 1
